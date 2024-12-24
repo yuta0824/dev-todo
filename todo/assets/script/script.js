4b673dfcb6e65ef340ef3689e7d7c797f788ee87
yuta <yu.code.tech@gmail.com>
2024-12-24 14:13:10 +0900

    入力結果をJSで取得できるようにする

--- todo/assets/script/script.js
+++ todo/assets/script/script.js
@@ -14,5 +14,27 @@ buttonRegister.addEventListener("click", () => {
   };
 
   todoList.push(newTodoObject);
-  console.log(todoList);
+  const tableElement = document.querySelector("#todo-list");
+
+  todoList.forEach((todo) => {
+    const todoNameElement = document.createElement("td");
+    todoNameElement.textContent = todo.todoName;
+    todoNameElement.classList.add("border", "border-gray-300", "px-4", "py-2");
+
+    const personElement = document.createElement("td");
+    personElement.textContent = todo.person;
+    personElement.classList.add("border", "border-gray-300", "px-4", "py-2");
+
+    const deadlineElement = document.createElement("td");
+    deadlineElement.textContent = todo.deadline;
+    deadlineElement.classList.add("border", "border-gray-300", "px-4", "py-2");
+
+    const trElement = document.createElement("tr");
+    trElement.appendChild(todoNameElement);
+    trElement.appendChild(personElement);
+    trElement.appendChild(deadlineElement);
+
+    tableElement.appendChild(trElement);
+    console.log(tableElement);
+  });
 });
