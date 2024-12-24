5c8e603385640672b8929b7b47ab0d6f8f950efc
yuta <yu.code.tech@gmail.com>
2024-12-24 14:52:23 +0900

    処理を関数に切り出して、可読性を向上する。

--- todo/assets/script/script.js
+++ todo/assets/script/script.js
@@ -1,38 +1,57 @@
+/**
+ * 変数の初期化
+ */
 const todoList = [];
-
+const tableBodyElement = document.querySelector("#todo-list");
 const buttonRegister = document.querySelector("#button-register");
 
-buttonRegister.addEventListener("click", () => {
+/**
+ * 入力されたTODOを取得する関数
+ *
+ * @returns {void}
+ */
+const registerNewTodo = () => {
   const newTodoName = document.querySelector("#new-todo-name");
   const newPerson = document.querySelector("#new-person");
   const newDeadline = document.querySelector("#new-deadline");
 
-  const newTodoObject = {
+  todoList.push({
     todoName: newTodoName.value,
     person: newPerson.value,
     deadline: newDeadline.value,
-  };
+  });
+};
 
-  todoList.push(newTodoObject);
-
-  const tableBodyElement = document.querySelector("#todo-list");
-
+/**
+ * 既存のTODO一覧を削除する関数
+ *
+ * @returns {void}
+ */
+const deleteTodoList = () => {
   while (tableBodyElement.firstChild) {
     tableBodyElement.firstChild.remove();
   }
+};
 
+/**
+ * TODOの一覧を描写する関数
+ *
+ * @param {string[]} classes - クラス属性の配列
+ * @returns {void}
+ */
+const renderTodoListElement = (classes) => {
   todoList.forEach((todo) => {
     const todoNameElement = document.createElement("td");
     todoNameElement.textContent = todo.todoName;
-    todoNameElement.classList.add("border", "border-gray-300", "px-4", "py-2");
+    todoNameElement.classList.add(...classes);
 
     const personElement = document.createElement("td");
     personElement.textContent = todo.person;
-    personElement.classList.add("border", "border-gray-300", "px-4", "py-2");
+    personElement.classList.add(...classes);
 
     const deadlineElement = document.createElement("td");
     deadlineElement.textContent = todo.deadline;
-    deadlineElement.classList.add("border", "border-gray-300", "px-4", "py-2");
+    deadlineElement.classList.add(...classes);
 
     const trElement = document.createElement("tr");
     trElement.appendChild(todoNameElement);
@@ -41,4 +60,15 @@ buttonRegister.addEventListener("click", () => {
 
     tableBodyElement.appendChild(trElement);
   });
+};
+
+/**
+ * 登録ボタンがクリックされたときの処理
+ *
+ * @returns {void}
+ */
+buttonRegister.addEventListener("click", () => {
+  deleteTodoList();
+  registerNewTodo();
+  renderTodoListElement(["border", "border-gray-300", "px-4", "py-2"]);
 });
